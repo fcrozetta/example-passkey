22912f93131965d88b341c1342773ec1439fac88
Fernando Crozetta <fernando@czetta.com>
2024-02-06 19:41:47 +0000

    nothing is working as it should D:

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,11 @@ from pydantic import BaseModel
 
 from webauthn import (
     generate_registration_options,
+    generate_authentication_options,
     verify_registration_response,
+    verify_authentication_response,
     options_to_json,
-    base64url_to_bytes,
+    base64url_to_bytes
 )
 from webauthn.helpers.cose import COSEAlgorithmIdentifier
 from webauthn.helpers.structs import (
@@ -26,6 +28,11 @@ origins = [
 ]
 
 
+# RP_ID = "localhost"
+RP_ID = "github.dev"
+
+USER_CHALLENGE = bytes([1,2,3,4,5,6,7,8,9,0])
+USER_PK = "TUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFZERMZmg4MjF5TGpKdVl3d01MSVhaQVJUaU9LMHFLdm52bG1aeE1KRmdJeWVkZnFMMkVreXNBeThHdFVjbUdiOXQ5VWpaMHZmWUUwUnUwVl9PVmx3eFE="
 
 app = FastAPI()
 app.add_middleware(
@@ -47,10 +54,45 @@ async def get_root():
 @app.post("/auth/register")
 async def start_register(user:registerBase):
     simple_registration_options = generate_registration_options(
-    rp_id="localhost",
+    rp_id=RP_ID,
     rp_name="Fcrozetta Dev",
     user_name=user.username,
-    user_id=bytes([1,2,3,4])
+    user_id=bytes([1,2,3,4]),
+    challenge=USER_CHALLENGE,
+    supported_pub_key_algs=[COSEAlgorithmIdentifier.ECDSA_SHA_512],
+    
 )
     # print(simple_registration_options)
     return json.loads(options_to_json(simple_registration_options))
+
+@app.post("/auth/register/validate")
+async def register_validate(credential:dict):
+    # Demonstrating the ability to handle a plain dict version of the WebAuthn response
+    registration_verification = verify_registration_response(
+        credential=credential,
+        expected_challenge=USER_CHALLENGE,
+        expected_origin="http://localhost:5000",
+        expected_rp_id="localhost",
+        require_user_verification=True
+    )
+
+@app.get("/auth/login/options")
+async def auth_options():
+    simple_authentication_options = generate_authentication_options(rp_id=RP_ID,challenge=USER_CHALLENGE)
+    bytes
+    return json.loads(options_to_json(simple_authentication_options))
+
+@app.post("/auth/login")
+async def auth_options(credential:str):
+    print(credential)
+    
+    simple_authentication_options = verify_authentication_response(
+        credential=credential,
+        expected_challenge=USER_CHALLENGE,
+        expected_rp_id=RP_ID,
+        expected_origin="https://miniature-trout-q5jx57wvjw29qqx-5173.app.github.dev",
+        credential_public_key=base64url_to_bytes(USER_PK),
+        credential_current_sign_count=0,
+        require_user_verification=True,
+    )
+    return json.loads(options_to_json(simple_authentication_options))
--- frontend/src/routes/passkey/+page.svelte
+++ frontend/src/routes/passkey/+page.svelte
@@ -1,52 +1,93 @@
 <script lang="ts">
-	import { startRegistration } from '@simplewebauthn/browser';
+	import { startRegistration, startAuthentication } from '@simplewebauthn/browser';
 	import { onMount } from 'svelte';
-	let username: string;
-	let server_options:string;
+	let server = 'https://miniature-trout-q5jx57wvjw29qqx-8000.app.github.dev';
+	// let server = 'http://localhost:8000';
+
+	let username: string = 'fer';
+	let registerOptions: string;
+	let AuthOptions: any;
 
 	onMount(() => {
-		updateServerOptions();
+		updateServerOptions(username);
 	});
 
-	async function updateServerOptions(){
-	console.log(JSON.stringify({"username":"fer"}))
-		fetch('http://127.0.0.1:8000/auth/register',{method:"POST",headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }, body:JSON.stringify({"username":"fer"})})
+	//  REGISTER
+	async function updateServerOptions(username: string) {
+		console.log(JSON.stringify({ username: username }));
+		fetch(server + '/auth/register', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ username: 'fer' })
+		})
 			// Why do I need those 2 "then" ??
 			.then((response) => response.json())
 			.then((rt) => {
-				server_options = JSON.stringify(rt);
+				registerOptions = JSON.stringify(rt);
 			});
 	}
 
-
-	function registration() {
-
-
-		console.log(server_options);
-		try{
-			let att = startRegistration(JSON.parse(server_options));
-		}catch (error) {
-			conole.error(error)
+	async function registration() {
+		await updateServerOptions(username);
+		try {
+			let att = startRegistration(JSON.parse(registerOptions));
+			console.log(att);
+		} catch (error) {
+			console.error(error);
 		}
 	}
 
-	function validate(){
+	// VALIDATE
+	async function updateAuthOptions() {
+		await fetch(server + '/auth/login/options')
+			// Why do I need those 2 "then" ??
+			.then((response) => response.json())
+			.then((rt) => {
+				AuthOptions = rt;
+			});
 	}
+	async function validate() {
+		// Request Challenge
+		if (!AuthOptions) {
+			await updateAuthOptions();
+		}
+		let asseResp;
+		try {
+			// Pass the options to the authenticator and wait for a response
+			asseResp = await startAuthentication(AuthOptions, true);
+			console.log("ASSERESP")
+			console.log(JSON.stringify(asseResp));
+		} catch (error) {
+			// Some basic error handling
+			console.error(error);
+			throw error;
+		}
 
+		const verificationResp = await fetch(server + '/auth/login', {
+			method: 'POST',
+			// mode: 'no-cors',
+			// headers: {
+			// 	'Content-Type': 'application/json'
+			// },
+			body: JSON.stringify(asseResp)
+		}).catch(err => {
+			console.error(err)
+		});
+	}
 </script>
 
 <h1>HELLO WORLD</h1>
 
-<h2> server options </h2>
-<input type="textarea" bind:value={server_options}>
-<button on:click={updateServerOptions}>Update Server options</button>
+<h2>server options</h2>
+<input type="textarea" bind:value={registerOptions} />
+<button on:click={updateServerOptions(username)}>Update Server options</button>
 
 <h2>Register new passkey</h2>
 <label for="username">Username</label>
-<input type="text" bind:value={username} />
+<input type="text" autocomplete="webauthn" bind:value={username} />
 <button on:click={registration}>Register</button>
 
 <h3>Validate User</h3>
