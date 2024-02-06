5ff900ecaf1113950c744b39f1f2481acd4c82bb
Fernando Crozetta <Fernando.Crozetta@rabobank.com>
2024-02-06 18:25:13 +0100

    CORS + simple request

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from webauthn import (
     generate_registration_options,
     verify_registration_response,
@@ -15,18 +18,39 @@ from webauthn.helpers.structs import (
     ResidentKeyRequirement,
 )
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class registerBase(BaseModel):
+    username:str
+
 
 @app.get("/")
 async def get_root():
     return "I'm alive!"
 
-@app.post("/register")
-async def start_register():
+@app.post("/auth/register")
+async def start_register(user:registerBase):
     simple_registration_options = generate_registration_options(
-    rp_id="fcrozetta.dev",
+    rp_id="localhost",
     rp_name="Fcrozetta Dev",
-    user_name="bob",
+    user_name=user.username,
+    user_id=bytes([1,2,3,4])
 )
     # print(simple_registration_options)
-    return options_to_json(simple_registration_options)
+    return json.loads(options_to_json(simple_registration_options))
