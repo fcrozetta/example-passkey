752c0bbd2c0a50c714bf4965da79ffba387ebc71
Fernando Crozetta <Fernando.Crozetta@rabobank.com>
2024-02-06 18:25:34 +0100

    i need help. and coffee
    mostly coffee

--- frontend/src/routes/passkey/+page.svelte
+++ frontend/src/routes/passkey/+page.svelte
@@ -1,31 +1,53 @@
 <script lang="ts">
 	import { startRegistration } from '@simplewebauthn/browser';
+	import { onMount } from 'svelte';
 	let username: string;
+	let server_options:string;
 
-  //Got this from python server
-	let server_options = "{\"rp\": {\"name\": \"Fcrozetta Dev\", \"id\": \"github.dev\"}, \"user\": {\"id\": \"iG3gLVSHCBvO88FNdtuzFPAIm0CFi3RHDsVQsCZ1W4po5tVmIFyiE1fyZkVVSCK5\", \"name\": \"bob\", \"displayName\": \"bob\"}, \"challenge\": \"9PhlrOg8NWifa9op7sYxp7prkImmIdUrVkumvGBlzUvYSDKvE31BtaZhDkKj6fjdlsEjTyG-p7-yWdEHFSCCoA\", \"pubKeyCredParams\": [{\"type\": \"public-key\", \"alg\": -7}, {\"type\": \"public-key\", \"alg\": -8}, {\"type\": \"public-key\", \"alg\": -36}, {\"type\": \"public-key\", \"alg\": -37}, {\"type\": \"public-key\", \"alg\": -38}, {\"type\": \"public-key\", \"alg\": -39}, {\"type\": \"public-key\", \"alg\": -257}, {\"type\": \"public-key\", \"alg\": -258}, {\"type\": \"public-key\", \"alg\": -259}], \"timeout\": 60000, \"excludeCredentials\": [], \"attestation\": \"none\"}"
+	onMount(() => {
+		updateServerOptions();
+	});
 
-	function test() {
-		alert(username);
+	async function updateServerOptions(){
+	console.log(JSON.stringify({"username":"fer"}))
+		fetch('http://127.0.0.1:8000/auth/register',{method:"POST",headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }, body:JSON.stringify({"username":"fer"})})
+			// Why do I need those 2 "then" ??
+			.then((response) => response.json())
+			.then((rt) => {
+				server_options = JSON.stringify(rt);
+			});
 	}
-  function getServerOptions(){
-    fetch("http://localhost:8000/register").then(value => console.log(value.body))
-  }
+
 
 	function registration() {
-    let parsed_data = JSON.parse(server_options)
-		let att = startRegistration(parsed_data)
-    console.log(parsed_data);
-    
-  
+
+
+		console.log(server_options);
+		try{
+			let att = startRegistration(JSON.parse(server_options));
+		}catch (error) {
+			conole.error(error)
+		}
 	}
+
+	function validate(){
+	}
+
 </script>
 
 <h1>HELLO WORLD</h1>
 
-<h2>Register new passkey</h2>
-<!-- <label for="username">Username</label> -->
-<!-- <input type="text" bind:value={username} /> -->
-<button on:click={registration}>Register</button>
-<button on:click={getServerOptions}>Debug</button>
+<h2> server options </h2>
+<input type="textarea" bind:value={server_options}>
+<button on:click={updateServerOptions}>Update Server options</button>
 
+<h2>Register new passkey</h2>
+<label for="username">Username</label>
+<input type="text" bind:value={username} />
+<button on:click={registration}>Register</button>
+
+<h3>Validate User</h3>
+<button on:click={validate}>Validate</button>
